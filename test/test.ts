a871699baa904436c2e5926af593319b7a15a554
Phil Kunz <gitlab@philkunz.com>
2017-09-17 17:00:56 +0200

    initial

---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import { expect, tap } from 'tapbundle'
+import * as smartexpress from '../ts/index'
+
+tap.test('first test', async () => {
+  console.log(smartexpress.standardExport)
+})
+
+tap.start()
