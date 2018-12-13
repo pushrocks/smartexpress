9c7f3323394318c9cc2c14193863e3a480b1e26f
Phil Kunz <github@philkunz.com>
2018-12-13 02:29:45 +0100

    fix(core): update

--- ts/smartexpress.classes.handlerstatic.ts
+++ ts/smartexpress.classes.handlerstatic.ts
@@ -6,15 +6,19 @@ export class HandlerStatic extends Handler {
   constructor(pathArg: string) {
     super("GET", async (req, res) => {
       const filePath: string = req.params.filePath;
+      const joinedPath = plugins.path.join(pathArg, filePath);
+
+      // important security checks
       if (
-        filePath.includes('..') ||
-        filePath.includes('~')
+        filePath.includes('..') || // don't allow going up the filePath
+        filePath.includes('~') || // don't allow referencing of home directory
+        !joinedPath.startsWith(pathArg) // make sure the joined path is within the directory
       ) {
         res.status(500);
         res.end();
         return;
       }
-      const fileString = plugins.smartfile.fs.toStringSync(plugins.path.join(pathArg, filePath));
+      const fileString = plugins.smartfile.fs.toStringSync(joinedPath);
       res.send(fileString);
       res.end();
     });
