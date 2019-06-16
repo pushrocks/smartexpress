e15eaccff30478b7ead54c3a0c90bc86701ff45f
Phil Kunz <gitlab@philkunz.com>
2019-06-16 16:12:55 +0200

    fix(core): update

--- ts/smartexpress.tools.manifest.ts
+++ ts/smartexpress.tools.manifest.ts
@@ -4,6 +4,7 @@ export const setupManifest = async (expressInstanceArg: plugins.express.Applicat
   const smartmanifestInstance = new plugins.smartmanifest.SmartManifest(manifestArg);
   expressInstanceArg.get('/manifest.json', async (req,res) => {
     res.status(200);
+    res.type('json');
     res.write(smartmanifestInstance.jsonString());
     res.end();
   });
