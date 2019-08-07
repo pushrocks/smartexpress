cbc8241fef0192c97666841abbb9e73731a56b67
Phil Kunz <gitlab@philkunz.com>
2019-08-07 16:45:38 +0200

    fix(core): update

--- ts/smartexpress.tools.manifest.ts
+++ ts/smartexpress.tools.manifest.ts
@@ -1,11 +1,14 @@
 import * as plugins from './smartexpress.plugins';
 
-export const setupManifest = async (expressInstanceArg: plugins.express.Application, manifestArg: plugins.smartmanifest.ISmartManifestConstructorOptions) => {
+export const setupManifest = async (
+  expressInstanceArg: plugins.express.Application,
+  manifestArg: plugins.smartmanifest.ISmartManifestConstructorOptions
+) => {
   const smartmanifestInstance = new plugins.smartmanifest.SmartManifest(manifestArg);
-  expressInstanceArg.get('/manifest.json', async (req,res) => {
+  expressInstanceArg.get('/manifest.json', async (req, res) => {
     res.status(200);
     res.type('json');
     res.write(smartmanifestInstance.jsonString());
     res.end();
   });
-}
+};
